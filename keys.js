12350494d8670ee5793018972d8af65115af676b
Sberry5 <shannon.berryheller@gmail.com>
2017-11-12 14:04:27 -0600

    Attempting switch

--- keys.js
+++ keys.js
@@ -4,10 +4,11 @@ var twitterKeys = {
   consumer_key: 'WagJA1Pweqv9K26bM6bTjyboC',
   consumer_secret: 'AJdzB34ZElMvOjNpQr8PAYGnkb4BeK4f3FmrAgfLRYEWSJMVeO',
   access_token_key: '928770144893054976-VWH44CRV5wsvyKM3EtJfRHbJy4ish8I',
-  access_token_secret: '5JZNlKLa7kQX2jgktKQors0VECEQVX3HSiIkTGx9tLFfm',
+  access_token_secret: '5JZNlKLa7kQX2jgktKQors0VECEQVX3HSiIkTGx9tLFfm'
 }
 
 module.exports = twitterKeys;
+
 
 
 var spotifyKeys = {
--- liri.js
+++ liri.js
@@ -1,43 +1,97 @@
+//Require keys.js folder
+var keys = require('./keys.js');
+var request = require('request');
+
+/*--------Switch to run different calls depending on user input--------*/
+var a = process.argV
+switch(a){
+	case "movie-this":
+		ombdRun(argument);
+		break;
+	// case "my-tweets":
+	// 	twitterRun();
+	// 	break;
+	// case "spotify-this-song":
+	// 	spotifyRun();
+	// 	break;
+	// case default:
+	// 	console.log("Hey, jerk, you know what you are supposed to enter");
+	// 	break;
+var searchThis = []
+var command = process.argv[2];
+var argument = process.argv[3];
+//Loop through user input to grab the movie name. Code allows users to input multi-word movie names.
+for (var i = 3; i < process.argv.length; i++) {
+  searchThis += " " + process.argv[i]};
+
 /*--------Code to handle OMDB--------*/
 
+function omdbRun(argument) {
 //Variable to 'require' request
-var request = require("request");
+//var request = require("request");
 
 //Variable to hold command line arguments
-var nodeArgs = process.argv;
+//var omdbArgs = process.argv;
 
 //Create an empty variable to hold the movie
-var movieName = "";
+// var movieName = argument;
 
 //Loop through user input to grab the movie name. Code allows users to input multi-word movie names.
-for (var i = 2; i < nodeArgs.length; i++) {
-  if (i > 2 && i < nodeArgs.length) {
-    movieName = movieName + "+" + nodeArgs[i];
-  }
-  else {
-    movieName += nodeArgs[i];
-  }
-}
+// for (var i = 2; i < omdbArgs.length; i++) {
+//   if (i > 2 && i < omdbArgs.length) {
+//     movieName = movieName + "+" + omdbArgs[i];
+//   }
+//   else {
+//     movieName += omdbArgs[i];
+//   }
+// }
 
 var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
 //console.log(queryUrl);
 
 request(queryUrl, function(error, response, body) {
 
-// If the request is successful
+//If the request is successful
   if (!error && response.statusCode === 200) {
-// Parse the body of the site and recover just the imdbRating
-// (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-    console.log("Title:" + JSON.parse(body).Title + "\nRelease Year: " + JSON.parse(body).Year + "\nCountry Produced: " + JSON.parse(body).Country + "\nLanguage: " + JSON.parse(body).Language + "\nPlot: " + JSON.parse(body).Plot + "\nActors " + JSON.parse(body).Actors);
+
+//Parse through data returned from API and show required data to user
+    console.log("Title:" + JSON.parse(body).Title + "\nRelease Year: " + JSON.parse(body).Year + "\nIMDB Rating: " + JSON.parse(body).Ratings[0].Value + "\nRotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value + "\nCountry Produced: " + JSON.parse(body).Country + "\nLanguage: " + JSON.parse(body).Language + "\nPlot: " + JSON.parse(body).Plot + "\nActors " + JSON.parse(body).Actors);
 	};
-});
+  });
+};
 
-//How to go into object and grab values when the attribute name is the smae
-//"\nIMDB Rating: " + JSON.parse(body).Ratings.(Source: Internet Moive Database).Value)
+// /*--------Code to handle Twitter--------*/
+//function twitterRun(){
 
-/*--------Code to handle Twitter--------*/
-var Spotify = require('node-spotify-api')
+// //Variable to 'require' the spotify npm
+// var Spotify = require('node-spotify-api', 'keys.js')
 
 
+// //Variable to hold command line arguments
+// var spotifyArgs = process.argv;
 
-/*--------Code to handle Spotify--------*/
+// //Create an empty variable to hold the movie
+// var songName = "";
+
+// // var spotifyKeys = require('keys.js');
+// // console.log(spotifyKeys);
+
+// var spotify = new Spotify (
+// 	 'spotifyKeys'
+// );
+// // console.log(spotifyKeys);
+
+// spotify
+//   .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
+//   .then(function(data) {
+//     console.log(data); 
+//   })
+//   .catch(function(err) {
+//     console.error('Error occurred: ' + err); 
+//   });
+
+};
+
+// /*--------Code to handle Spotify--------*/
+
+//function spotifyRun(){
